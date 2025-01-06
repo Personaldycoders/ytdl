a13caa158a7ded1a14d733dfc6424bb091064085
Personaldycoders <xyydycoders@gmail.com>
2025-01-06 18:16:40 +0700

    Fix JSON syntax error in package.json

--- api/youtube.js
+++ api/youtube.js
@@ -1,5 +1,5 @@
 const { youtube } = require('btch-downloader');
-
+const cheerio = require('cheerio');
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
